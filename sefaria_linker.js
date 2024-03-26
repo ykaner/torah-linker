a630524644747924e9fc233900c58e122293227e
Yosef Kaner <ykaner12@gmail.com>
2024-03-26 21:37:38 +0200

    Run the linker on all the pages.

---
+++ sefaria_linker.js
@@ -0,0 +1,13 @@
+
+window.addEventListener('load', function () {
+    (async () => {
+        const linkerScriptUrl = chrome.runtime.getURL('js/linker.v3.js');
+        const linker = await import(linkerScriptUrl);
+        linker.linkerFunction();
+        sefaria.link({
+            mode: 'popup-click',
+            contentLang: "hebrew",
+            interfaceLang: "hebrew"
+        });
+    })();
+});
